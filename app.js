f4bbebd288faa85aca5c56c71dd9e592e86ce3f2
pengxiaobao <a924230386@qq.com>
2016-06-26 23:55:49 +0800

    add login

--- app.js
+++ app.js
@@ -22,8 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
+app.use('/', routes.index);
+// app.use('/login', routes.login);
+// app.use('/login', routes.doLogin);
+// app.use('/logout', routes.logout);
+// app.use('/home', routes.home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+var request = {};
 /* GET home page. */
-router.get('/', function(req, res, next) {
+request.index = router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+request.login = router.get('/login', function(req, res, next) {
+   res.render('login', {
+     title: '用户登陆'
+   });
+});
+
+request.doLogin = router.post('/doLogin', function(req, res) {
+  var user = {
+    username: 'admin',
+    password: 'admin'
+  }
+
+  if (req.body.username === user.username && req.body.password === user.password) {
+    res.redirect('/home');
+  }
+  res.redirect('/login');
+});
+
+request.logout = router.get('/logout', function(req, res) {
+  res.redirect('/');
+});
+
+request.logout = router.get('/home', function(req, res) {
+  var user = {
+    username: 'admin',
+    password: 'admin'
+  }
+  res.render('home', {
+    title: 'Home',
+    user: user
+  });
+});
+
+module.exports = request;
